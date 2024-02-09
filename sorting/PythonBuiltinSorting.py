8cedc0f323272321380fe085fd57585cd4649999
Hariom Gupta <102603759+codehariom@users.noreply.github.com>
2024-02-09 08:12:55 +0530

    Sorting

---
+++ sorting/PythonBuiltinSorting.py
@@ -0,0 +1,17 @@
+A = [63,250,835,947,651,28]
+print('Original List:', A)
+A.sort()
+print('Sorted List:', A)
+
+A = [63,250,835,947,651,28]
+print('Original List:', A)
+B = sorted(A)
+print('List-A:', A)
+print('List-B:', B)
+
+A = [63,250,835,947,651,28]
+print('Original List:', A)
+A.sort(reverse=True)
+print('Sorted List:', A)
+
+
